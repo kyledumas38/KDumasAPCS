913ea8e9e03d5d8a15f418cd73ea1d888062f68a
Kyle Dumas <Dumas.Kyle2019@ssusd.org>
2018-02-05 07:40:55 -0800

    variables

---
+++ Operators/Operators.java
@@ -0,0 +1,16 @@
+class Operators {
+    public static void main(String args[]) {
+    //String name = "kyle";
+    //System.out.println(name);
+        String firstName = "kyle";
+    String lastName = "dumas";
+    String fullName = firstName + " " + lastName;
+
+    System.out.println(firstName);
+    System.out.println("kyle");
+    System.out.println(lastName);
+    System.out.println("dumas");
+    System.out.println(fullName);
+    System.out.println("kyle dumas");
+    }
+}
