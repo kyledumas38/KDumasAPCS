b7e5830aeb7221ac263e18a7e21af1d60299247d
Kyle Dumas <Dumas.Kyle2019@ssusd.org>
2018-02-15 07:34:47 -0800

    loan calculator.java

--- Operators/Operators.java
+++ Operators/Operators.java
@@ -1,16 +1,25 @@
 class Operators {
     public static void main(String args[]) {
-    //String name = "kyle";
-    //System.out.println(name);
-        String firstName = "kyle";
-    String lastName = "dumas";
-    String fullName = firstName + " " + lastName;
+    
+     Boolean isDesert = true;
+    Boolean haveAC = true;
+    Boolean haveSwampCooler = false;
 
-    System.out.println(firstName);
-    System.out.println("kyle");
-    System.out.println(lastName);
-    System.out.println("dumas");
-    System.out.println(fullName);
-    System.out.println("kyle dumas");
+    if(isDesert) {
+        if(haveAC) {
+            System.out.println("Your electric bill is high.");
+        }
+        else if(haveSwampCooler) {
+            System.out.println("Your electric bill isn't too bad.");
+            System.out.println("Your house gets humid.");
+        }
+        else {
+            //Default case
+            System.out.println("How do you survive here?");
+        }
     }
+    else {
+        //Default case
+        System.out.println("Your electric bill might be good or bad, but I'm not sure.");
+    
 }
